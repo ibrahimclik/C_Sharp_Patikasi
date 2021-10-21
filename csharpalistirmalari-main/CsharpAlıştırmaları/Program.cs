3d7b9944b5a81b42467fd7895fc665d9436e2c2d
ibrahimclik <91675264+ibrahimclik@users.noreply.github.com>
2021-10-21 08:23:06 +0300

    Add files via upload

---
+++ csharpalistirmalari-main/CsharpAlıştırmaları/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpAlıştırmaları
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
