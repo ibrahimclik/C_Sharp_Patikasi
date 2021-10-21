11154f4727069f5486f8a9b8d9cf8b1d463a1636
ibrahimclik <91675264+ibrahimclik@users.noreply.github.com>
2021-10-21 08:25:36 +0300

    Add files via upload

---
+++ forloopdeneme-main/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace switchcasedeneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //0 İle Girlen Sayı arasındaki çift sayıları ekrana yansıtır. 
+            Console.WriteLine("Lütfen Bir Sayı Giriniz");
+            int sayac=int.Parse(Console.ReadLine());
+            for (int i = 0; i < sayac; i++)
+            {
+                if(i%2==0)
+                Console.WriteLine(i);
+            }
+
+
+            int length=1000;
+            int csayı=0;
+            int tsayı=0;
+            
+            //0 ile 1000 arasındaki tep ve çift sayıların toplamı.
+
+            for (int i = 0; i <= length; i++)
+            {
+                if(i%2==0)
+                csayı=csayı+i;
+                
+            }
+            Console.WriteLine("Çift Sayıların Toplamı=" + csayı);
+            
+             for (int i = 0; i <= length; i++)
+            {
+                if(i%2!=0)
+                tsayı=tsayı+i;
+                
+            }
+            Console.WriteLine("Tek Sayıların Toplamı=" + tsayı);
+             
+            //Break Ve Continue Keyworldleri
+
+            //Koşula bağlı şekilde döngüden çıkmak istiyorsa break kullanırız.
+            //Koşula bağlı kalarak döngüyü o kuşulda es geçmek için kullanırız.
+
+            for (int i = 1; i < 10; i++)
+            {
+                if(i==4)
+                break;
+                Console.WriteLine(i);
+                //Burada break komutu sayesinde 4 olunca looptan çıktı.
+            }
+            
+                for (int i = 1; i < 10; i++)
+            {
+                if(i==4)
+                continue;
+                Console.WriteLine(i);
+                //Burada continue komutu sayesinde 4 olunca loopu atladı.
+            }
+             
+        }
+
+    }
+}
