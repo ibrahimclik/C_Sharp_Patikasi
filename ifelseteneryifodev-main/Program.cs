246867c6eeba83d2ba6418a9e28b68aec4093d9d
ibrahimclik <91675264+ibrahimclik@users.noreply.github.com>
2021-10-21 08:25:10 +0300

    Add files via upload

---
+++ ifelseteneryifodev-main/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ifelsealıştırma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (DateTime.Now.Hour<=18)
+            
+            { 
+                Console.WriteLine("İyi Günler");   
+            
+             }
+
+            else if (DateTime.Now.Hour <=11 && DateTime.Now.Hour >=06)
+            
+            { 
+                Console.WriteLine("Günaydın");
+                 }
+
+            else 
+
+            { 
+                Console.WriteLine("İyi Geceler"); 
+                } 
+            
+            //Burada aynı işlemi yapan kodu farklı bir şekilde de yazabilir.
+              
+
+            //Sadece iki durum olan için
+
+            string sonuc1=DateTime.Now.Hour <=18 ? "iyi Günler":"İyi Geceler";
+            Console.WriteLine(sonuc1);
+            
+            //İkiden fazla durum için.
+
+            string sonuc=DateTime.Now.Hour >=06 && DateTime.Now.Hour <=11 ? "Günaydın" :DateTime.Now.Hour>=18 ? "İyi Geceler":"İyi Günler";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
---
+++ ifelseteneryifodev-main/README.md
@@ -0,0 +1,1 @@
+# ifelseteneryifodev
